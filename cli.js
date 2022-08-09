d68fbd2ace0ed6888c3198444a76b5d57669a0cb
Lyssa <lyssa.lima@poli.digital>
2022-08-09 17:19:45 -0300

    feat: remover console.log e criar nova função

--- cli.js
+++ cli.js
@@ -1,4 +1,13 @@
-import getFile from './index.js';
 //executar usando node cli.js ./files/post.md
+import getFile from './index.js';
+import chalk from 'chalk';
+
 const path = process.argv;
-console.log(getFile(path[2]));
+
+async function processText(filePath){
+    const result = await getFile(path[2]);
+    console.log(chalk.yellow('Lista de Links localizados no arquivo:'), result);
+
+}
+
+processText(path);
