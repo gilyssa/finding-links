bd082420b256dfcfc40ffb52ad6f0a514b48ae20
Lyssa <lyssa.lima@poli.digital>
2022-08-16 11:38:51 -0300

    feat: adicionar valiação de links

--- cli.js
+++ cli.js
@@ -1,13 +1,17 @@
 //executar usando node cli.js ./files/post.md
 import getFile from './index.js';
 import chalk from 'chalk';
+import validationURLs from './http-validation.js';
 
 const path = process.argv;
 
 async function processText(filePath){
     const result = await getFile(path[2]);
-    console.log(chalk.yellow('Lista de Links localizados no arquivo:'), result);
-
+    if(path[3] === 'validate'){
+        console.log(chalk.yellow('Links Validados'), await validationURLs(result));
+    } else {
+        console.log(chalk.yellow('Lista de Links localizados no arquivo:'), result);
+    }
 }
 
 processText(path);
